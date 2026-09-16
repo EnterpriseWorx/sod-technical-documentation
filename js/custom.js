14b9f87b95cd7abeaeddc3b11144fa3c49cf5c41
Michael van Antwerpen <michael.antwerpen@ewx.co.za>
2026-09-16 12:37:45 +0200

    Deployed cc9e56b with ProperDocs version: 1.6.7

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  //wrap all tables in a div with table-responsive class and make them striped
+  $("table").wrap("<div class='table-responsive'></div>");
+  $("table").addClass("table-striped");
+});
